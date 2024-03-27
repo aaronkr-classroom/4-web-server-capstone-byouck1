78fa4b82cee341f4bc0f2a093e87e8928192a717
byouck1 <163220417+byouck1@users.noreply.github.com>
2024-03-27 03:16:16 +0000

    6주차끝(1)

---
+++ content-types.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// p.112 listening 7.4
+module.exports={
+    html:{
+        "Content-Type": "text/html"
+    },
+    text: {
+        "Content-Type": "text/plain"
+    },
+    css: {
+        "Content-Type": "text/css"
+    },
+    js: {
+        "Content-Type": "text/javascript"
+    },
+    jpg: {
+        "Content-Type": "text/jpg"
+    },
+    png: {
+        "Content-Type": "text/png"
+    }
+};
---
+++ utils.js
@@ -0,0 +1,20 @@
+"using strict";
+
+
+const fs=require('fs'),
+    httpStatus=require('http-status-codes'),
+    contentTypes=require('./content-types');
+
+module.exports={
+    getFile:(file,res) => {
+        fs.readFile(`./${file}`,(error,data)=> {
+            if (error) {
+                res.writeHead(httpStatus.INTERNAL_SERVER_ERROR,
+                contentTypes.html       
+            );
+                res.end("error serving content!!!!");
+            };
+            res.end(data);
+        });
+    }
+}
